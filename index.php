c4450be4ff28db5523d06641ec659d7e55a085b8
Josselin <josselin.arpin@gmail.com>
2020-09-25 13:32:17 +0200

    Add elements to index page

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 
 ?>
 
-<html>
-
-Index Page !
-
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Index !</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+	Index
+</body>
 </html>
